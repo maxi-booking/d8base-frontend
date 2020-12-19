f60b2cfd480e9e99def632245c6eac90b63ed8c8
Slepukhin Vitalii <vitslepukhin@gmail.com>
2020-12-19 23:00:39 +0400

    feat(order): Redirect to login page and come back

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,31 +1,31 @@
-import {HttpErrorResponse} from '@angular/common/http';
-import {Component, OnDestroy} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {Credentials} from '@app/auth/interfaces/credentials';
-import {AuthenticationFactory} from '@app/core/services/authentication-factory.service';
-import {MasterManagerService} from '@app/core/services/master-manager.service';
-import {Subject} from 'rxjs';
-import {filter, takeUntil} from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Credentials } from '@app/auth/interfaces/credentials';
+import { AuthenticationFactory } from '@app/core/services/authentication-factory.service';
+import { MasterManagerService } from '@app/core/services/master-manager.service';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.page.html',
     styleUrls: ['./login.page.scss']
 })
-export class LoginPage implements OnDestroy {
+export class LoginPage implements OnInit, OnDestroy {
     public errorMessage: string;
     private readonly destroy$ = new Subject<void>();
+    private redirectTo: string = '/profile';
 
     constructor(
         private readonly authFactory: AuthenticationFactory,
         private readonly router: Router,
-        route: ActivatedRoute,
+        private readonly route: ActivatedRoute,
         private readonly masterManager: MasterManagerService
-    ) {
-        route.queryParams.pipe(
-            filter(params => params?.hasOwnProperty('logout')),
-            takeUntil(this.destroy$)
-        ).subscribe(() => this.logout());
+    ) {}
+
+    public ngOnInit(): void {
+        this.subscribeRouteParams();
     }
 
     public ngOnDestroy(): void {
@@ -34,25 +34,49 @@ export class LoginPage implements OnDestroy {
     }
 
     public onSubmitLoginForm(user: Credentials): void {
-        this.authFactory.getAuthenticator().login(user).subscribe(
-            _ => {
-                this.masterManager.updateIsMaster();
-                this.router.navigateByUrl('/profile');
-            },
-            (error: HttpErrorResponse) => {
-                if (400 === error.status && error.error.error === 'invalid_grant') {
-                    this.errorMessage = 'login-page.incorrect-login-data';
-                } else {
-                    throw error;
+        this.authFactory
+            .getAuthenticator()
+            .login(user)
+            .subscribe(
+                () => {
+                    this.masterManager.updateIsMaster();
+                    this.router.navigateByUrl(this.redirectTo);
+                },
+                (error: HttpErrorResponse) => {
+                    if (400 === error.status && error.error.error === 'invalid_grant') {
+                        this.errorMessage = 'login-page.incorrect-login-data';
+                    } else {
+                        throw error;
+                    }
                 }
-            }
-        );
+            );
     }
 
     private logout(): void {
-        this.authFactory.getAuthenticator().logout().subscribe(() => {
-            this.masterManager.updateIsMaster();
-            this.router.navigateByUrl('/auth/login');
-        });
+        this.authFactory
+            .getAuthenticator()
+            .logout()
+            .subscribe(() => {
+                this.masterManager.updateIsMaster();
+                this.router.navigateByUrl('/auth/login');
+            });
+    }
+
+    private subscribeRouteParams(): void {
+        this.route.queryParams
+            .pipe(
+                filter(params => params?.hasOwnProperty('logout')),
+                takeUntil(this.destroy$)
+            )
+            .subscribe(() => this.logout());
+
+        this.route.queryParams
+            .pipe(
+                filter(params => params?.hasOwnProperty('redirectTo')),
+                takeUntil(this.destroy$)
+            )
+            .subscribe(({ redirectTo }) => {
+                this.redirectTo = redirectTo;
+            });
     }
 }
--- src/app/service/components/service-details-page/service-details-page.component.ts
+++ src/app/service/components/service-details-page/service-details-page.component.ts
@@ -1,32 +1,55 @@
-import {Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {ProfessionalList} from '@app/api/models';
-import {ServicesReadonlyApiService} from '@app/core/services/services-readonly-api.service';
-import {MasterReadonlyApiService} from '@app/master/services/master-readonly-api.service';
-import {Service} from '@app/service/models/service';
-import {first, switchMap, tap} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProfessionalList } from '@app/api/models';
+import { AuthenticationService } from '@app/core/services/authentication.service';
+import { ServicesReadonlyApiService } from '@app/core/services/services-readonly-api.service';
+import { MasterReadonlyApiService } from '@app/master/services/master-readonly-api.service';
+import { Service } from '@app/service/models/service';
+import { first, switchMap, withLatestFrom } from 'rxjs/operators';
 
 @Component({
     selector: 'app-service-details-page',
     templateUrl: './service-details-page.component.html',
     styleUrls: ['./service-details-page.component.scss']
 })
-export class ServiceDetailsPageComponent {
-
+export class ServiceDetailsPageComponent implements OnInit {
     public service: Service;
 
     public master: ProfessionalList;
+    public orderRouterLink: string;
+    public orderRouterQueryParams: any;
 
     constructor(
-        route: ActivatedRoute,
-        servicesApi: ServicesReadonlyApiService,
-        masterApi: MasterReadonlyApiService
-    ) {
-        route.params.pipe(
-            first(params => Boolean(params?.id)),
-            switchMap(params => servicesApi.getByEntityId(params.id)),
-            tap(service => this.service = service),
-            switchMap(service => masterApi.getByEntityId(service.professional))
-        ).subscribe(master => this.master = master);
+        private readonly route: ActivatedRoute,
+        private readonly servicesApi: ServicesReadonlyApiService,
+        private readonly masterApi: MasterReadonlyApiService,
+        private readonly authenticationService: AuthenticationService
+    ) {}
+
+    public ngOnInit(): void {
+        this.subscribeRouteParams();
+    }
+
+    private subscribeRouteParams(): void {
+        this.route.params
+            .pipe(
+                first(params => Boolean(params?.id)),
+                switchMap(params => this.servicesApi.getByEntityId(params.id)),
+                withLatestFrom(this.authenticationService.isAuthenticated$),
+                switchMap(([service, isAuthenticated]) => {
+                    this.service = service;
+                    const orderLink = `/order/${service.id}`;
+                    if (isAuthenticated) {
+                        this.orderRouterLink = orderLink;
+                        this.orderRouterQueryParams = null;
+                    } else {
+                        this.orderRouterLink = `/auth/login`;
+                        this.orderRouterQueryParams = { redirectTo: orderLink };
+                    }
+
+                    return this.masterApi.getByEntityId(service.professional);
+                })
+            )
+            .subscribe(master => (this.master = master));
     }
 }
